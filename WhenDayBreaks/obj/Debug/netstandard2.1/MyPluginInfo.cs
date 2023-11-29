4acf5f3908ee905705505f6e3edae667b2f595e6
Guartis <104228518+Guartis@users.noreply.github.com>
2023-11-29 22:45:38 +0100

    Add files via upload

---
+++ WhenDayBreaks/obj/Debug/netstandard2.1/MyPluginInfo.cs
@@ -0,0 +1,9 @@
+namespace When Day Breaks
+{
+    public static class MyPluginInfo
+    {
+        public const string PLUGIN_GUID = "When Day Breaks";
+        public const string PLUGIN_NAME = "When Day Breaks";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
